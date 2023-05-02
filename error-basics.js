8fd0ccf6dfd7af59662f7957bd4b4875606646eb
christokarsky <christokarsky1@gmail.com>
2023-05-02 16:31:57 -0500

    initial commit

---
+++ error-basics.js
@@ -0,0 +1,31 @@
+//errors with try, throw, and catch
+
+try {
+    throw "Here's an error!"
+} catch (thrownError){
+    console.log(`Uh oh! ${thrownError}`)
+}
+
+// = Uh oh! Here's an error!
+
+function getCarColor(car) {
+    try {
+      if (!car){
+        throw `Color unknown`
+      } else {
+        return car.color
+      }
+    } catch (errorThrown){
+      return errorThrown
+    }
+  }
+  
+  let car = { make: "Ford", model: "Mustang", color: "Bullet Grey" };
+  
+  console.log(getCarColor(undefined))
+
+  // = Color unknown
+
+  console.log(getCarCOlor(car))
+
+  // = Bullet Grey
